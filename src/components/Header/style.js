e22f89048e6cd6177e58fa262af210c4b9af587e
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-08 20:16:19 -0300

    creating and style header file

---
+++ src/components/Header/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Header = styled.header `
+
+    h1, h4 {
+      
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 10px;
+    
+    }
+
+`
