f792a020138dae70da1294921e7ef57afd6c15b5
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-08 20:20:58 -0300

    Using head custom tag component inside the Header/index.jsx file

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,11 @@ import { Head } from './style'
 export const Header = () => (
 
 
-    <>
-    
-    </>
+    <Head>
+
+        <h1>GENEPASS</h1>
+        <h4>The Password Generator</h4>
+
+    </Head>
 
 )
