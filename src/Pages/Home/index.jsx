8018f5dc98d20edd86326f29cf344e8e7986e8b7
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-08 20:26:02 -0300

    import Header component on Home Page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 import { Button } from '../../components/Button' 
+import { Header } from './components/Header/index'
 
 import { View, Container, Options } from './styles'
 
@@ -42,6 +43,8 @@ export const Home = () => {
     return (
 
         <Container>
+
+            <Header />
 
             <View type = "text">
                 
