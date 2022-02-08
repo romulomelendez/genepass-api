1b9839e3ff958fae6dab2a00e78ca6b90762057f
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-08 20:29:32 -0300

    changing import path to Header component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import { Button } from '../../components/Button' 
-import { Header } from './components/Header/index'
+import { Header } from '../../components/Header'
 
 import { View, Container, Options } from './styles'
 
