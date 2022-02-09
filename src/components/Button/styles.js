16131348e683374666f785499b88e4c8057bcf0f
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2022-02-08 21:06:11 -0300

    Button component style update

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -2,20 +2,16 @@ import styled from 'styled-components'
 
 export const Btn = styled.button `
 
-    button { 
+    padding: 10px;
+    height: 50px;
+    width: 200px;
+    border: 1px solid black;
+    border-radius: 10px;
+    margin-left: 610px;
+    background-color: white;
+    font-weight: bold;
+    cursor: pointer;
 
-        padding: 10px;
-        height: 50px;
-        width: 200px;
-        border: 1px solid black;
-        border-radius: 10px;
-        margin-left: 610px;
-        background-color: white;
-        font-weight: bold;
-        cursor: pointer;
-        
-    }
-
-    button:hover { background-color: #cecece; }
+    :hover { background-color: #cecece; }
 
 `
