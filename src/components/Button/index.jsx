51d17f4d1ce72ebf0933cd6f7a94a8b5c8a5e490
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-11-02 20:47:08 -0300

    inital commit in new branch

--- src/components/Button/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-import { Btn } from './styles'
-
-export const Button = ({ onClick }) => <Btn type = "button" onClick = { onClick }>CREATE PASSWORD!</Btn>
--- src/components/CheckboxesContainer/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Section = styled.section `
-        
-    display: flex;
-    flex-direction: column;
-    
-`
