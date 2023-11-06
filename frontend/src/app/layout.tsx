4040303da5ade7d62c7b9b8a2063a315391f296a
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-11-06 17:16:59 -0300

    🛠️ Fix: Next.js Warning: "Extra attributes from the server" solved

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={inter.className}
+        suppressHydrationWarning={true}>
+          {children}
+      </body>
     </html>
   )
 }
