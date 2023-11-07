9751f349ee5ebbd70d421696a7d2b3d8d98c6319
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-11-07 02:10:08 -0300

    Feat: password( fake, for now) is appearing on password display

--- frontend/src/components/PasswordDisplay.tsx
+++ frontend/src/components/PasswordDisplay.tsx
@@ -1,8 +1,18 @@
+"use client"
+
+import { usePwd } from "@/hooks/usePwd"
+
 const PasswordDisplay: React.FC = () => {
+
+    const { password } = usePwd()
 
     return (
         <section>
-            <input type="text" className="p-2 h-15 sm:h-28 w-64 sm:w-full text-violet-700 text-2xl sm:text-5xl rounded-2xl outline-none text-center bg-slate-900" />
+            <input
+              type="text"
+              className="p-2 h-15 sm:h-28 w-64 sm:w-full text-violet-700 text-2xl sm:text-5xl rounded-2xl outline-none text-center bg-slate-900"
+              value={password}
+            />
         </section>
     )
 }
