fa85e31ec08979135c882bbe32e8be6116b7b9fd
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-11-07 23:18:08 -0300

    Feat: Copy to clipboard function is done 📋

--- frontend/src/components/PasswordDisplay.tsx
+++ frontend/src/components/PasswordDisplay.tsx
@@ -3,19 +3,27 @@
 import { usePwd } from "@/hooks/usePwd"
 
 import { BsFillClipboardFill } from "react-icons/bs"
+import copy from 'copy-to-clipboard'
 
 const PasswordDisplay: React.FC = () => {
 
     const { password } = usePwd()
+
+    const handleCopy = (): void | Boolean | string => password && copy(password)
 
     return (
         <section className="flex justify-between p-1 h-15 sm:h-28 w-64 sm:w-fit max-w-xl rounded-2xl bg-slate-900">
             <input
               type="text"
               className="bg-slate-900 h-full w-52 sm:w-full p-4 text-violet-700 text-2xl sm:text-5xl outline-none text-center"
-            //   value={password}
+              value={password}
+              readOnly
             />
-            <button type="button" className="bg-slate-900 flex justify-center items-center p-1 mr-1 h-full w-12">
+            <button
+              type="button"
+              className="bg-slate-900 flex justify-center items-center p-1 mr-1 h-full w-12"
+              onClick={handleCopy}
+            >
                 <BsFillClipboardFill size="22" />
             </button>
         </section>
