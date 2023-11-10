909980e716e930e784fe844d86177d225cf15089
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-11-10 20:33:44 -0300

    little server update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,4 +2,4 @@ import { app } from './app'
 
 const PORT = process.env.PORT || 3333
 
-app.listen(PORT, () => console.log("🚀 Server listening on port 3333"))
+app.listen(PORT, () => console.log(`🚀 Server listening on port ${PORT}`))
