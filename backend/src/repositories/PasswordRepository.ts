7f68ec731923a306787944518ce1528a7216b85d
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2023-11-10 20:47:10 -0300

    feat: creating attributes

--- backend/src/repositories/PasswordRepository.ts
+++ backend/src/repositories/PasswordRepository.ts
@@ -3,6 +3,11 @@ import { pwdBodyProps } from '../types'
 
 export class PasswordRepository implements iPasswordRepository {
 
+    private symbols: string[] = ["!", "@", "?", "-", "_", "=", "+", "*", "<", ">", ".", ";", ":", "{", "}", "[", "]", "#", "$", "%", "&"]
+    private numbers: string[]= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    private capitalLetter: string[]= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    private smallLetter: string[]= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
     public create(body: pwdBodyProps): string {
         
         return ""
