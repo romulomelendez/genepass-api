3ca39692b01d3de27a5b9cb78cea67ec7a70ed29
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-08 00:39:02 -0300

    little update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -8,7 +8,7 @@ import { PasswordProvider } from '@/providers/PasswordProvider'
 
 const Home: React.FC = async () => (
   <div className="bg-slate-900 text-white sm:w-screen h-screen flex justify-center items-center">
-    <div className="bg-slate-950 w-screen sm:w-3/4 h-screen p-5 flex flex-col justify-around items-center gap-10 rounded-md">
+    <div className="bg-slate-950 w-screen sm:w-3/4 h-screen p-5 flex flex-col justify-around items-center rounded-md">
       <PasswordProvider>
         <Header />
         <PasswordDisplay />
