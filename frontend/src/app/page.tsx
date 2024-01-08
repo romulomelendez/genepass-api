b373e7ae17cf906f537f692b4eb12958339c3e5f
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-08 00:35:15 -0300

    feat: creating a footer to link github

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,21 @@
 import Header from '../components/Header'
 import PasswordDisplay from '../components/PasswordDisplay'
-import ActionButtons from '../components/ActionButtons'
-import CreatePwdBtn from '../components/CreatePwdBtn'
+import ActionButtons from '@/components/ActionButtons'
+import CreatePwdBtn from '@/components/CreatePwdBtn'
+import Footer from '@/components/Footer'
 
-import { PasswordProvider } from '../providers/PasswordProvider'
+import { PasswordProvider } from '@/providers/PasswordProvider'
 
 const Home: React.FC = async () => (
   <div className="bg-slate-900 text-white sm:w-screen h-screen flex justify-center items-center">
-    <div className="bg-slate-950 w-screen sm:w-3/4 h-screen p-5 flex flex-col justify-center items-center gap-10 rounded-md">
+    <div className="bg-slate-950 w-screen sm:w-3/4 h-screen p-5 flex flex-col justify-around items-center gap-10 rounded-md">
       <PasswordProvider>
         <Header />
         <PasswordDisplay />
         <ActionButtons />
         <CreatePwdBtn />
       </PasswordProvider>
+      <Footer />
     </div>
   </div>
 )
