cd5efcdb0343a3acf83e30f26f9df75431e402a6
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-14 01:34:41 -0300

    little update

--- backend/src/repositories/PasswordRepository.ts
+++ backend/src/repositories/PasswordRepository.ts
@@ -5,15 +5,22 @@ export class PasswordRepository implements iPasswordRepository {
 
     private symbols: string[] = ["!", "@", "?", "-", "_", "=", "+", "*", "<", ">", ".", ";", ":", "{", "}", "[", "]", "#", "$", "%", "&"]
     private numbers: string[]= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    private capitalLetter: string[]= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    private smallLetter: string[]= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    private capitalLetters: string[]= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    private smallLetters: string[]= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     public create(body: pwdBodyProps): string {
         
+        const pwdLength = +body.length
+
+        const optionsArr = this.createArrayWithOptions(body)
+
         return ""
     }
 
-    // public getPwdLength = (body: pwdBodyProps): number => {
-    //     return Number(body.length)
-    // }
+    public createArrayWithOptions(body: pwdBodyProps) {
+
+        switch(body) {
+            case
+        }
+    }
 }
