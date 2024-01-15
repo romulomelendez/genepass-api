5e087fea5c7d2af1e529ce43c7d28451f617fb66
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-01-15 06:39:37 -0300

    🧪backend: creating some tests

--- backend/src/tests/create-password.spec.ts
+++ backend/src/tests/create-password.spec.ts
@@ -4,11 +4,27 @@ import { CreatePasswordRepository } from "../infra/repositories/create-password"
 
 describe("Create Password", () => {
 
-    test("It should create a password with just symbols and lenght 8", () => {
+    test("It should create a password with just one preference", () => {
 
         const pwdSpecsObj: PasswordBody = {
-            length: 12,
-            symbols: false,
+            length: 10,
+            symbols: true,
+            numbers: false,
+            capitalLetters: false,
+            smallLetters: false
+        }
+
+        let password = new CreatePasswordRepository().execute(pwdSpecsObj)
+
+        expect(typeof password).toBe("string")
+        expect(password).toHaveLength(pwdSpecsObj.length)
+    })
+
+    test("It should create a password with just two preference", () => {
+
+        const pwdSpecsObj: PasswordBody = {
+            length: 10,
+            symbols: true,
             numbers: true,
             capitalLetters: false,
             smallLetters: false
@@ -16,6 +32,39 @@ describe("Create Password", () => {
 
         let password = new CreatePasswordRepository().execute(pwdSpecsObj)
 
+        expect(typeof password).toBe("string")
+        expect(password).toHaveLength(pwdSpecsObj.length)
+    })
+
+    test("It should create a password with just three preference", () => {
+
+        const pwdSpecsObj: PasswordBody = {
+            length: 10,
+            symbols: true,
+            numbers: true,
+            capitalLetters: true,
+            smallLetters: false
+        }
+
+        let password = new CreatePasswordRepository().execute(pwdSpecsObj)
+
+        expect(typeof password).toBe("string")
+        expect(password).toHaveLength(pwdSpecsObj.length)
+    })
+
+    test("It should create a password with all preference", () => {
+
+        const pwdSpecsObj: PasswordBody = {
+            length: 10,
+            symbols: true,
+            numbers: true,
+            capitalLetters: true,
+            smallLetters: true
+        }
+
+        let password = new CreatePasswordRepository().execute(pwdSpecsObj)
+
+        console.log(password)
         expect(typeof password).toBe("string")
         expect(password).toHaveLength(pwdSpecsObj.length)
     })
