11c2087c5fe11cbb1e4397a9e53964c0bf7c8fe4
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-02-21 07:35:31 -0300

    backend: little update to remove the test route

--- backend/src/routes/password-routes.ts
+++ backend/src/routes/password-routes.ts
@@ -17,7 +17,3 @@ router.post("/api/createPwd", ({ body }: Request, res: Response) => {
         message
     })
 })
-
-router.get("/", (req: Request, res: Response) => {
-    res.status(200).json("OK")
-})
