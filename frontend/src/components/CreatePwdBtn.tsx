6e9d19936dc2d68cdd6cdf902eae09178316316e
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-02-27 17:52:19 -0300

    little sample .env update

--- frontend/src/components/CreatePwdBtn.tsx
+++ frontend/src/components/CreatePwdBtn.tsx
@@ -10,9 +10,9 @@ const CreatePwdBtn = () => {
 
         <section className="w-auto sm:w-96 flex justify-center sm:justify-end">
             <button
-                type="button"
-                className="bg-violet-800 w-max p-4 rounded-2xl text-lg hover:bg-violet-900"
-                onClick={createPwd}
+              type="button"
+              className="bg-violet-800 w-max p-4 rounded-2xl text-lg hover:bg-violet-900"
+              onClick={createPwd}
             >
                 Create Password
             </button>
