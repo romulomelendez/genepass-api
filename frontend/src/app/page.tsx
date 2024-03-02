3e43a675a47e5944552d4684cca4ee36e3d32818
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-03-02 17:08:17 -0300

    fix: removing footer and making responsible update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,23 +1,21 @@
-import Header from '@/components/Header'
-import PasswordDisplay from '@/components/PasswordDisplay'
-import ActionButtons from '@/components/ActionButtons'
-import CreatePwdBtn from '@/components/CreatePwdBtn'
-import Footer from '@/components/Footer'
+import ActionButtons from "@/components/ActionButtons";
+import CreatePwdBtn from "@/components/CreatePwdBtn";
+import Header from "@/components/Header";
+import PasswordDisplay from "@/components/PasswordDisplay";
 
-import { PasswordProvider } from '@/providers/PasswordProvider'
+import { PasswordProvider } from "@/providers/PasswordProvider";
 
 const Home: React.FC = async () => (
-  <div className="bg-slate-900 text-white sm:w-screen h-screen flex justify-center items-center">
-    <div className="bg-slate-950 w-screen sm:w-3/4 h-screen p-5 flex flex-col justify-around items-center rounded-md">
-      <PasswordProvider>
-        <Header />
+  <div className="flex flex-col h-screen gap-2 items-center justify-evenly bg-slate-950">
+    <PasswordProvider>
+      <Header />
+      <div className="flex flex-col gap-5">
         <PasswordDisplay />
         <ActionButtons />
         <CreatePwdBtn />
-      </PasswordProvider>
-      <Footer />
-    </div>
+      </div>
+    </PasswordProvider>
   </div>
-)
+);
 
-export default Home
+export default Home;
--- frontend/src/components/CreatePwdBtn.tsx
+++ frontend/src/components/CreatePwdBtn.tsx
@@ -1,25 +1,19 @@
-"use client"
+"use client";
 
-import { usePwd } from "../hooks/usePwd"
+import { usePwd } from "../hooks/usePwd";
 
 const CreatePwdBtn = () => {
+  const { createPwd } = usePwd();
 
-    const { createPwd } = usePwd()
+  return (
+      <button
+        type="button"
+        className="bg-violet-800 w-full p-4 rounded-2xl text-lg hover:bg-violet-900"
+        onClick={createPwd}
+      >
+        Create Password
+      </button>
+  );
+};
 
-    return (
-
-        <section className="w-auto sm:w-96 flex justify-center sm:justify-end">
-            <button
-              type="button"
-              className="bg-violet-800 w-max p-4 rounded-2xl text-lg hover:bg-violet-900"
-              onClick={createPwd}
-            >
-                Create Password
-            </button>
-        </section>
-
-    )
-
-}
-
-export default CreatePwdBtn
+export default CreatePwdBtn;
