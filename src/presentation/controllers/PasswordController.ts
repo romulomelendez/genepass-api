fd33de11238d362dc74e376b113ed26f93fbc802
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-12-14 10:40:35 -0300

    🆙 UPDATE: Changing the controller name to a more appropriate name

--- src/presentation/controllers/PasswordController.ts
+++ src/presentation/controllers/PasswordController.ts
@@ -3,14 +3,13 @@ import { PasswordBody } from "../../domain/models"
 import { CreatePasswordRepository } from "../../infra/repositories/create-password"
 import { HttpHelper } from "../helpers"
 
-export class CreatePasswordController implements Controller {
+export class PasswordController implements Controller {
     
-    constructor(private readonly createPasswordRepo: CreatePasswordRepository) {}
-
-    // @ts-ignore
+    constructor(private readonly passwordRepository: CreatePasswordRepository) {}
+   
     handle = (pwdBody: PasswordBody) => {
 
-        const password = this.createPasswordRepo.execute(pwdBody)
+        const password = this.passwordRepository.execute(pwdBody)
 
         if (!password)
             return HttpHelper.INTERNAL_SERVER_ERROR("⚠️ Internal Server Error")
--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -2,5 +2,5 @@ import { PasswordBody } from "../../domain/models"
 import { HttpResponse } from "./http-response"
 
 export interface Controller<T = any> {
-    handle(params: PasswordBody): Promise<HttpResponse<T>>
+    handle(params: PasswordBody): HttpResponse<T>
 }
