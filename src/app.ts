166299ff411d1383831780d5968e3b6bce5f9895
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-12-20 00:31:33 -0300

    cors another config update

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,28 @@
 import express, { json } from "express"
 import cors from "cors"
 
-import { CorsConfig } from './utils'
-
 import { router } from "./routes"
 
 const app = express()
 
-app.use(cors(CorsConfig))
+const allowedOrigins = [
+    'http://localhost:8080',
+    'http://localhost:3000',
+    'https://genepass-ui.vercel.app'
+]
+
+app.use(cors({
+    origin: (origin, callback) => {
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+}))
+
 app.use(json())
 app.use(router)
 
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,1 @@
 export * from './PasswordUtils'
-export * from './corsConfig'
