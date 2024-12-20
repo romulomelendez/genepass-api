3076f638ee1c78df3d48dd6c8783e694130c1fa2
Rômulo Melendez Lima <romulomelendez.97@gmail.com>
2024-12-19 23:44:32 -0300

    🆙 UPDATE: update in cors config to accept request to front-end deployed

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,8 @@ import { router } from "./routes"
 const app = express()
 
 app.use(cors({
-    origin: 'http://localhost:8080'
+    origin: ['http://localhost:8080', 'https://genepass-ui.vercel.app/'],
+    methods: 'POST'
 }))
 app.use(json())
 app.use(router)
